6f899f9f87dfab22006c75d5a6910a7e50028f79
Iryna Kruk <iryna_kruk@epam.com>
2016-04-01 08:10:01 +0300

    weak cash map implementation

--- WeakCache/src/main/java/com/weak/cache/Main.java
+++ WeakCache/src/main/java/com/weak/cache/Main.java
@@ -4,9 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.util.LinkedList;
 import java.util.Random;
-import java.util.WeakHashMap;
 
 public class Main {
 
@@ -15,22 +13,21 @@ public class Main {
 	private static final String FILE_PATH = "D:\\pdp\\test\\";
 
 	public static void main(String[] args) {
-		WeakCache weakCache = new WeakCache(new LinkedList<String>(),
-				new WeakHashMap<String, byte[]>());
-		//Main.createFiles();
+		WeakCache weakCache = new WeakCache();
+		// Main.createFiles();
 		Main.populateCache(weakCache);
 	}
 
 	public static void populateCache(WeakCache cache) {
 		Random random = new Random();
-		while (true) {
-			String fileName = String.valueOf(random.nextInt(MAX_SIZE_OF_ELEMENTS + 1));
+		for (int i = 0; i < MAX_SIZE_OF_ELEMENTS; i++) {
+			String fileName = String.valueOf(random
+					.nextInt(MAX_SIZE_OF_ELEMENTS) + 1);
 			try (RandomAccessFile file = new RandomAccessFile(FILE_PATH
 					+ fileName, "r")) {
-				cache.addElemToHardCache(fileName);
 				byte[] bytes = new byte[(int) file.length()];
 				file.read(bytes);
-				cache.getWeakCache().put(fileName, bytes);
+				cache.put(fileName, bytes);
 				System.out.println(cache);
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
@@ -41,10 +38,10 @@ public class Main {
 	}
 
 	public static void createFiles() {
-		for (int i = 1; i <= 100; i++) {
+		for (int i = 1; i <= MAX_SIZE_OF_ELEMENTS; i++) {
 			File file = new File(FILE_PATH + i);
 			try {
-				if(file.createNewFile()){
+				if (file.createNewFile()) {
 					System.out.println("file created");
 				}
 			} catch (IOException e) {
--- WeakCache/src/main/java/com/weak/cache/WeakCache.java
+++ WeakCache/src/main/java/com/weak/cache/WeakCache.java
@@ -1,31 +1,21 @@
 package com.weak.cache;
 
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.WeakHashMap;
 
-public class WeakCache {	
+public class WeakCache implements Map<String, Object> {
 
 	private static final int MAX_SIZE_HARD_CACHE = 30;
 
-	private List<String> hardCache;
-	private Map<String, byte[]> weakCache;	
+	private List<String> hardCache = new LinkedList<String>();
+	private Map<String, Object> weakCache = new WeakHashMap<String, Object>();
 
-	public WeakCache(List<String> hardCache, Map<String, byte[]> weakCache) {
-		super();
-		this.hardCache = hardCache;
-		this.weakCache = weakCache;
-	}
-
-	public List<String> getHardCache() {
-		return hardCache;
-	}
-
-	public Map<String, byte[]> getWeakCache() {
-		return weakCache;
-	}
-	
-	public void addElemToHardCache(String element){
-		if(hardCache.size()<MAX_SIZE_HARD_CACHE){
+	private void addElemToHardCache(String element) {
+		if (hardCache.size() < MAX_SIZE_HARD_CACHE) {
 			hardCache.add(element);
 		} else {
 			hardCache.remove(0);
@@ -37,5 +27,71 @@ public class WeakCache {
 	public String toString() {
 		return "WeakCache [hardCache=" + hardCache + ", weakCache=" + weakCache
 				+ "]";
+	}
+
+	@Override
+	public int size() {
+		return weakCache.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return weakCache.isEmpty();
+	}
+
+	@Override
+	public boolean containsKey(java.lang.Object key) {
+		return weakCache.containsKey(key);
+	}
+
+	@Override
+	public boolean containsValue(java.lang.Object value) {
+		return weakCache.containsValue(value);
+	}
+
+	@Override
+	public Object get(java.lang.Object key) {
+		return weakCache.get(key);
+	}
+
+	@Override
+	public Object put(String key, Object value) {
+		addElemToHardCache(key);
+		return weakCache.put(key, value);
+	}
+
+	@Override
+	public Object remove(java.lang.Object key) {
+		hardCache.remove(key);
+		return weakCache.remove(key);
+	}
+
+	@Override
+	public void putAll(Map<? extends String, ? extends Object> m) {
+		weakCache.putAll(m);
+		for(String key : m.keySet()){
+			addElemToHardCache(key);
+		}
+	}
+
+	@Override
+	public void clear() {
+		hardCache.clear();
+		weakCache.clear();
+	}
+
+	@Override
+	public Set<String> keySet() {
+		return weakCache.keySet();
+	}
+
+	@Override
+	public Collection<Object> values() {
+		return weakCache.values();
+	}
+
+	@Override
+	public Set<java.util.Map.Entry<String, Object>> entrySet() {		
+		return weakCache.entrySet();
 	}
 }
